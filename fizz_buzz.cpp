b7ce274804bb62de5b2feb17f3d527749fa0185d
GEOEGII555 <geoegii2001555@gmail.com>
2025-02-13 12:01:41 +0300

    Remove useless comment

--- fizz_buzz.cpp
+++ fizz_buzz.cpp
@@ -35,6 +35,5 @@ FizzBuzzResult operator&(FizzBuzzResult lhs, FizzBuzzResult rhs) {
 tostringstream& operator<<(tostringstream& ostream, FizzBuzzResult const& fizzBuzzResult) {
 	if ((fizzBuzzResult & FizzBuzzResult::FIZZ) != FizzBuzzResult::UNKNOWN) ostream << TEXT("Fizz");
 	if ((fizzBuzzResult & FizzBuzzResult::BUZZ) != FizzBuzzResult::UNKNOWN) ostream << TEXT("Buzz");
-	// if ((fizzBuzzResult & FizzBuzzResult::FIZZ) != FizzBuzzResult::UNKNOWN) ostream << ostream.str().substr(0, ostream.str().find_first_of(TEXT(':')));
 	return ostream;
 }
